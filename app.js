cdf6a12604f877fa95efc3c9a1b3c45ac508e301
fatima <fatimahassanhassab8@gmail.com>
2024-04-12 15:12:24 -0700

    first commit for api

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+ const cors=require('cors');
+const app = express();
+
+
+// const PORT = process.env.PORT || 3030;
+ app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/services',function(req,res){
+//      var services=[
+//           {
+//                title:"service 1 -x",
+//                description:"description1"
+// },
+//           {
+//                title:"service 2",
+//                description:"description2"
+     
+//           },
+//           {
+//                title:"service 3",
+//                description:"description3"
+     
+//           },
+         
+//           {
+//                title:"service 4",
+//                description:"description4"
+     
+//           },
+//           {
+//                title:"service 5",
+//                description:"description5"
+     
+//           },
+//           {
+//                title:"service 6",
+//                description:"description6"
+     
+//           }];
+          // res.send(services);
+          const xlsx=require('xlsx');
+const wb=xlsx.readFile('data.xlsx');
+let ws=wb.Sheets['Sheet1'];
+          var data=xlsx.utils.sheet_to_json(ws);
+          res.send(data);
+})
+
+app.listen(3000);
